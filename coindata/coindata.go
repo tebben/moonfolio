c6aa77b66f11a70a2db052a4537db687cd759d8c
time <ebben.tim@gmail.com>
2018-01-14 20:10:44 +0100

    Work on runner and transactions to coindata

--- coindata/coindata.go
+++ coindata/coindata.go
@@ -9,25 +9,45 @@ import (
 type CoinData struct {
 	Name         string
 	Symbol       string
-	Rank         int32
+	Rank         int
 	PriceUSD     float64
 	HistoHour    *cryptocompare.Histo
 	HistoDay     *cryptocompare.Histo
-	transactions []transactions.Transaction
+	transactions []*transactions.Transaction
 }
 
-func (c *CoinData) Update() {
-
-}
-
-func (c *CoinData) SetTransactions() {
+func (c *CoinData) Update(price, histoHour, histoDay bool) {
 
 }
 
-func (c *CoinData) GetTransactions() {
-
+func (c *CoinData) SetName(name string) {
+	c.Name = name
 }
 
-func (c *CoinData) AddTransaction() {
+func (c *CoinData) SetSymbol(symbol string) {
+	c.Symbol = symbol
+}
 
+func (c *CoinData) SetRank(rank int) {
+	c.Rank = rank
+}
+
+func (c *CoinData) SetPriceUSD(price float64) {
+	c.PriceUSD = price
+}
+
+func (c *CoinData) SetTransactions(transactions []*transactions.Transaction) {
+	c.transactions = transactions
+}
+
+func (c *CoinData) GetTransactions() []*transactions.Transaction {
+	return c.transactions
+}
+
+func (c *CoinData) AddTransaction(transaction *transactions.Transaction) {
+	if c.transactions == nil {
+		c.transactions = make([]*transactions.Transaction, 0)
+	}
+
+	c.transactions = append(c.transactions, transaction)
 }
--- runner/runner.go
+++ runner/runner.go
@@ -3,39 +3,70 @@ package runner
 import (
 	"log"
 
+	"strconv"
+
+	"github.com/tebben/moonfolio/coindata"
 	"github.com/tebben/moonfolio/configuration"
 	"github.com/tebben/moonfolio/cryptocompare"
 	"github.com/tebben/moonfolio/transactions"
 )
 
 var (
-	coinList        *cryptocompare.CoinList
-	transactionList []*transactions.Transaction
+	allCoins  *cryptocompare.CoinList
+	userCoins map[string]*coindata.CoinData
+	config    *configuration.Config
 )
 
 // Start starts the runner
-func Start(config *configuration.Config) {
-	// get coin list
-
-	// get transaction
+func Start(c *configuration.Config) {
+	config = c
+	getCoinList()
+	createCoinData()
 }
 
 func getCoinList() {
 	c, err := cryptocompare.GetCoinList()
 	if err != nil {
-
+		// ToDo set error in interface
 	}
 
-	coinList = c
+	allCoins = c
 }
 
-func getTransactions() {
-	t, err := transactions.GetTransactions()
+func createCoinData() {
+	ta, err := transactions.GetTransactions()
 	if err != nil {
 		log.Fatalf("unable to get transactions: %v", err)
 	}
 
-	transactionList = t
+	userCoins = make(map[string]*coindata.CoinData, 0)
+
+	// add transaction to a user coin, create user coin if there is no coin yet
+	for _, t := range ta {
+		if uc, ok := userCoins[t.CoinID]; !ok {
+			uc = &coindata.CoinData{}
+
+			// not created, find coin in allCoins
+			if ccCoin, ok2 := allCoins.Data[t.CoinID]; ok2 {
+				uc.SetName(ccCoin.FullName)
+				uc.SetSymbol(ccCoin.Symbol)
+				rank, _ := strconv.Atoi(ccCoin.SortOrder)
+				uc.SetRank(rank)
+			} else { // coins from cryptocompare did not find the coin refered in the transaction
+				uc.SetName(t.CoinID)
+				uc.SetSymbol(t.CoinID)
+			}
+
+			uc.AddTransaction(t)
+			userCoins[uc.Symbol] = uc
+		} else {
+			uc.AddTransaction(t)
+		}
+	}
+}
+
+func update(price, histoHour, histoDay bool) {
+
 }
 
 // set error in GUI
